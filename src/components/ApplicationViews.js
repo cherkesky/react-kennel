c7405512c7bf0ef36711c017617bcb96b44e2aa4
Cherkesky <guy@cherkesky.com>
2019-11-20 11:40:54 -0600

    exercise 6 setup

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,14 +1,12 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
-//import AnimalCard from './animal/AnimalCard'
-//import LocationCard from './location/LocationCard'
-//import EmployeeCard from './employee/EmployeeCard'
 import OwnerCard from './owner/OwnerList'
 import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import AnimalForm from './animal/AnimalForm'
+import AnimalDetail from './animal/AnimalDetail'
 
 
 
@@ -18,23 +16,27 @@ class ApplicationViews extends Component {
   render() {
     // React.Fragment is the same as <> </>
     return (
-      <React.Fragment> 
+      <React.Fragment>
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-         <Route path="/animal" render={(props) => {
+        <Route path="/animal" render={(props) => {
           return <AnimalList />
+        }} />
+        <Route path="/animals/:animalId(\d+)" render={(props) => {
+          // Pass the animalId to the AnimalDetailComponent
+          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} />
         }} />
         <Route path="/location" render={(props) => {
           return <LocationList />
         }} />
-         <Route path="/employee" render={(props) => {
+        <Route path="/employee" render={(props) => {
           return <EmployeeList />
         }} />
-         <Route path="/owner" render={(props) => {
+        <Route path="/owner" render={(props) => {
           return <OwnerCard />
         }} />
-         <Route path="/form" render={(props) => {
+        <Route path="/form" render={(props) => {
           return <AnimalForm />
         }} />
       </React.Fragment>
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import '../animal/Animal.css';
+import { Link } from "react-router-dom";
+
 
 class AnimalCard extends Component {
   render() {
@@ -12,6 +14,7 @@ class AnimalCard extends Component {
           <h3>Name: <b>{this.props.animal.name}</b></h3>
           <p>Breed: {this.props.animal.breed}</p>
           <button type="button" onClick={() => this.props.deleteAnimal(this.props.animal.id)}>Discharge</button>
+          <Link to={`/animals/${this.props.animal.id}`}><button>Details</button></Link>
         </div>
       </div>
     );
