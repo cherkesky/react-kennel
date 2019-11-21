da3902fc26bbd94a3185ae865d05dafa95c46fe7
Cherkesky <guy@cherkesky.com>
2019-11-21 11:41:28 -0600

    fixed a routing bug from chapter 7

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -14,37 +14,46 @@ import LocationDetail from './location/LocationDetail'
 class ApplicationViews extends Component {
 
   render() {
-    // React.Fragment is the same as <> </>
     return (
       <React.Fragment>
+
         <Route exact path="/" render={(props) => {
           return <Home />
         }} />
-        <Route exact path="/animal" render={(props) => {
-          return <AnimalList />
+
+        {/***************************************************************************************/}
+        {/**********                           Animal Routing                            ********/}
+        {/***************************************************************************************/}
+        <Route exact path="/animals" render={(props) => {
+          return <AnimalList {...props} />
         }} />
         <Route path="/animals/:animalId(\d+)" render={(props) => {
           // Pass the animalId to the AnimalDetailComponent \d+ is looking for digits
-          console.log("REACT PROPS",props)
-         // console.log("THIS PROPS",this.props)
-          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props}/>
+          console.log("REACT PROPS", props)
+          // console.log("THIS PROPS",this.props)
+          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props} />
         }} />
+        <Route path="/animals/new" render={(props) => {
+          return <AnimalForm {...props} />
+        }} />
+        {/***************************************************************************************/}
+        {/**********                           Location Routing                          ********/}
+        {/***************************************************************************************/}
         <Route path="/location" render={(props) => {
           return <LocationList />
         }} />
-         <Route path="/locations/:locationId(\d+)" render={(props) => {
+        <Route path="/locations/:locationId(\d+)" render={(props) => {
           // Pass the locationId to the LocationDetailComponent \d+ is looking for digits
           return <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props} />
         }} />
+        {/***************************************************************************************/}
         <Route path="/employee" render={(props) => {
           return <EmployeeList />
         }} />
         <Route path="/owner" render={(props) => {
           return <OwnerCard />
         }} />
-        <Route path="/form" render={(props) => {
-          return <AnimalForm />
-        }} />
+
       </React.Fragment>
     )
   }
